aca0af257d971dacfa540ed06f7a02fdaf248fb8
Eunsoo Ha <146503043+ha9eun@users.noreply.github.com>
2026-01-13 19:54:56 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.37 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < numLog.length; i++) {
+            int prev = numLog[i -1];
+            int next = numLog[i];
+            switch (next - prev) {
+                case 1 : sb.append('w');
+                    break;
+                case -1 : sb.append('s');
+                    break;
+                case 10 : sb.append('d');
+                    break;
+                case -10 : sb.append('a');
+                    break;
+            }
+           
+        }
+        
+        return sb.toString();
+    }
+}
