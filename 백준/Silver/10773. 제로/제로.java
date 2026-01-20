7cd3087184d1daaf2f0212ef28c366f0539f210f
Eunsoo Ha <146503043+ha9eun@users.noreply.github.com>
2026-01-20 16:53:34 +0900

    [Silver IV] Title: 제로, Time: 220 ms, Memory: 23992 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) {
+                stack.pop();
+            }
+            else {
+                stack.push(num);
+            }
+        }
+        int sum = 0;
+        while(!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+        br.close();
+    }
+}
